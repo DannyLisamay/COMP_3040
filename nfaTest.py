3e84e4b46f3b9bb2af80f9be3095abbc7788bac4
Danny Lisamay <dannylisamay@gmail.com>
2020-11-23 00:52:53 -0500

    Update to task 26

--- nfaTest.py
+++ nfaTest.py
@@ -23,59 +23,59 @@ oracle(NFA_NoStrings, ["0", "0"], False)
 #NFA Even length
 Print("NFA_Even_Length")
 #True
-oracle(NFA_Even_Length, [None, "q0"], True)
+oracle(NFA_Even_Length, ["q0", None], True)
 oracle(NFA_Even_Length, [], True)
-oracle(NFA_Even_Length, [["0","q1"], ["1", "q0"]], True)
+oracle(NFA_Even_Length, [["q1", "0"], ["q0", "1"]], True)
 #False
-oracle(NFA_Even_Length, ["0","q1"], False)
-oracle(NFA_Even_Length, [["0","q1"],["1", "q0"],["0", "q1"]], False)
+oracle(NFA_Even_Length, [["q1", "0"]], False)
+oracle(NFA_Even_Length, [["q1", "0"],["q0", "1"],["q1", "0"]], False)
 
 #NFA Odd length
 Print("NFA_Odd_Length")
 #True
-oracle(NFA_Odd_Length, ["0","q1"], True)
-oracle(NFA_Odd_Length, [["0","q1"],["1", "q0"],["0", "q1"]], True)
+oracle(NFA_Odd_Length, [["q1", "0"]], True)
+oracle(NFA_Odd_Length, [["q1", "0"],["q0", "1"],["q1", "0"]], True)
 #False
-oracle(NFA_Even_Length, [None, "q0"], False)
+oracle(NFA_Odd_Length, [["q0", None]], False)
 oracle(NFA_Odd_Length, [], False)
-oracle(NFA_Odd_Length, [["0","q1"], ["1", "q0"]], False)
+oracle(NFA_Odd_Length, [["q1", "0"], ["q0", "1"]], False)
 
 #NFA N1
 Print("NFA_N1")
 #True
-oracle(NFA_N1, [["1", "q1"], ["0", "q2"], ["1", "q3"]], True)
-oracle(NFA_N1, [["1", "q1"], [None, "q2"], ["1", "q3"]], True)
-oracle(NFA_N1, [["1", "q1"], ["0", "q2"], ["1", "q3"]], ["1", "q3"], True)
+oracle(NFA_N1, [["q1", "1"], ["q2", "0"], ["q3", "1"]], True)
+oracle(NFA_N1, [["q1", "1"], ["q2", None], ["q3", "1"]], True)
+oracle(NFA_N1, [["q1", "1"], ["q2", "0"], ["q3", "1"], ["q3", "1"]], True)
 #False
 oracle(NFA_N1, [], False)
-oracle(NFA_N1, [["1", "q0"], ["0", "q0"], ["1", "q0"]], False)
-oracle(NFA_N1, [["1","q1"],["0", "q2"]], False)
+oracle(NFA_N1, [["q0", "1"], ["q0", "0"], ["q0", "1"]], False)
+oracle(NFA_N1, [["q1", "1"],["q2", "0"]], False)
 
 #NFA N2
 Print("NFA_N2")
 #True
-oracle(NFA_N2, [["1", "q1"], ["0", "q2"], ["1", "q3"]], True)
-oracle(NFA_N2, [["1", "q1"], ["1", "q2"], ["0", "q3"]], True)
-oracle(NFA_N2, [["1", "q0"], ["1", "q1"], ["0", "q2"], ["1", "q3"]], True)
+oracle(NFA_N2, [["q1", "1"], ["q2", "0"], ["q3", "1"]], True)
+oracle(NFA_N2, [["q1", "1"], ["q2", "1"], ["q3", "0"]], True)
+oracle(NFA_N2, [["q0", "1"], ["q1", "1"], ["q2", "0"], ["q3", "1"]], True)
 #False
 oracle(NFA_N2, [], False)
-oracle(NFA_N2, [["1", "q0"], ["0", "q0"], ["1", "q0"]], False)
-oracle(NFA_N2, [["1","q0"],["1", "q1"]], False)
+oracle(NFA_N2, [["q0", "1"], ["q0", "0"], ["q0", "1"]], False)
+oracle(NFA_N2, [["q0", "1"],["q1", "1"]], False)
 
 #NFA N4
 Print("NFA_N4")
 #True
 oracle(NFA_N4, [], True)
-oracle(NFA_N4, [[None, "q2"], ["a", "q0"]], True)
-oracle(NFA_N4, [["b", "q1"], ["a", "q2"], ["a", "q0"]], True)
+oracle(NFA_N4, [["q2", None], ["q0", "a"]], True)
+oracle(NFA_N4, [["q1", "b"], ["q2", "a"], ["q0", "a"]], True)
 #False
-oracle(NFA_N4, [["b", "q1"]], False)
-oracle(NFA_N4, [["b", "q1"], ["a", "q2"]], False)
-oracle(NFA_N4, [["b", "q1"], ["a", "q1"], ["a", "q1"]], False)
+oracle(NFA_N4, [["q1", "b"]], False)
+oracle(NFA_N4, [["q1", "b"], ["q2", "a"]], False)
+oracle(NFA_N4, [["q1", "b"], ["q1", "a"], ["q1", "a"]], False)
 
 #NFA ThreeFromEndIsOne
 Print("NFA_ThreeFromEndIsOne")
-oracle(NFA_ThreeFromEndIsOne, [["0","1"], ["1","0"], ["1","0"], ["0","0"], ["1","0"]], False)
-oracle(NFA_ThreeFromEndIsOne, [["0","0"], ["1","0"], ["1","0"], ["0","0"], ["1","0"]], True)
-oracle(NFA_ThreeFromEndIsOne, [["0","0"], ["1","0"], ["1","1"], ["0","2"], ["1","2"]], True)
-oracle(NFA_ThreeFromEndIsOne, [["0","0"], ["1","0"], ["1","1"], ["0","2"], ["1","2"]], False)
+oracle(NFA_ThreeFromEndIsOne, [["1", "0"], ["0", "1"], ["0", "1"], ["0", "0"], ["0", "1"]], False)
+oracle(NFA_ThreeFromEndIsOne, [["0", "0"], ["0", "1"], ["0", "1"], ["0", "0"], ["0", "1"]], True)
+oracle(NFA_ThreeFromEndIsOne, [["0", "0"], ["0", "1"], ["1", "1"], ["2", "0"], ["2", "1"]], True)
+oracle(NFA_ThreeFromEndIsOne, [["0", "0"], ["0", "1"], ["1", "1"], ["2", "0"], ["2", "1"]], False)
