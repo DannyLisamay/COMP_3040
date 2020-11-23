5f874535a454f91ad326d06ff6d214536d159190
Danny Lisamay <dannylisamay@gmail.com>
2020-11-23 15:01:50 -0500

    Task #27 Completed. Had to modify NFAs and NFA Functions

--- nfa.py
+++ nfa.py
@@ -2,40 +2,51 @@ import dfa_ver2
 
 #*******TASK #23 ************
 # Define a data type to represent NFAs.
-# epsilon is "None"
 class NFA:
-    def __init__(self, statesFunction, alphabet, startState, transitionFunction, acceptFunction):
+    def __init__(self, statesFunction, alphabet, startState, transitionFunction, acceptFunction, epsilonFunction):
         self.statesFunction = statesFunction
         self.alpha = alphabet
         self.startState = startState
         self.transitionFunction = transitionFunction
         self.acceptFunction = acceptFunction
+        self.epsilonFunction = epsilonFunction
 
 
 #*******TASK #24 ************
 # Write a (trivial) function that converts DFAs into NFAs.
 # Pretty much what was gone over in class.
-# My delta is represented with a list, casted to tuple to work with my getAcceptedString function.
 def dfa2Nfa(dfa):
     def transitionFunction(qi, c):
-        if c == None:
-            return tuple([])
-        else:
-            return tuple([dfa.transitionFunction])
-    return DFA(dfa.statesFunction, dfa.alpha, dfa.startState, transitionFunction, dfa.acceptFunction)
+        return [dfa.transitionFunction]
+    def epsilonFunction(qi):
+        return []
+    return DFA(dfa.statesFunction, dfa.alpha, dfa.startState, transitionFunction, dfa.acceptFunction, epsilonFunction)
 
 
 #*******TASK #27 ************
 #Write a function that given an NFA, a string, a trace, and a boolean,
 #determines if the trace is a valid execution of the NFA and accepts results in the given boolean.
+
 def oracle(nfa, trace, result):
-    oracle_(nfa, nfa.startState, trace, res)
+    return oracle_(nfa, nfa.startState, trace, result)
 
 #Helper function
 def oracle_(nfa, qi, trace, result):
+    # Check if trace is empty
     if not trace:
-
-
+        return result == nfa.acceptFunction(qi)
+    # te is first element tp is rest
+    [te, *tp] = trace
+    def qjs():
+        # check if te is epsilon
+        if len(te) == 1:
+            return nfa.epsilonFunction(qi)
+        # else transition
+        else:
+            return nfa.transitionFunction(qi, te[1])
+    # Recursion
+    print(qi)
+    return te[0] in qjs() and oracle_(nfa, te[0], tp, result)
 #*******TASK #25 ************
 # Write a example NFAs.
 # casted to tuple to work with my getAcceptedString function. (was an issue with DFAs)
@@ -47,135 +58,172 @@ def NFA_NoStrings():
         lambda x: x == "0",
         ["0", "1"],
         "0",
-        lambda qi, c: "0" or None,
+        lambda qi, c: "0",
         lambda qi: False,
+        lambda qi: "0"
     )
 #** dfa accepts even string length
 def NFA_Even_Length():
     def transitionFunction(qi, c):
-        # If epsilon return currentState
-        if c == None:
-            return self.startState
-
         if qi == "q0":
-            return tuple(["q1"])
+            return ["q1"]
         else:
-            return tuple(["q0"])
-
+            return ["q0"]
+    def epsilonFunction(qi):
+        return qi
     return NFA(
         lambda qi: qi == "q0" or qi == "q1",
         ["0", "1"],
         "q0",
         transitionFunction,
-        lambda qi: qi == ["q0"]
+        lambda qi: qi == "q0",
+        epsilonFunction
     )
 
 #** dfa accepts odd string length
 def NFA_Odd_Length():
     def transitionFunction(qi, c):
-        # If epsilon return currentState
-        if c == None:
-            return self.startState
-
         if qi == "q0":
-            return tuple(["q1"])
+            return ["q1"]
         else:
-            return tuple(["q0"])
-
+            return ["q0"]
+    def epsilonFunction(qi):
+        return qi
     return NFA(
         lambda qi: qi == "q0" or qi == "q1",
         ["0", "1"],
         "q0",
         transitionFunction,
-        lambda qi: qi == "q1"
+        lambda qi: qi == "q1",
+        epsilonFunction
     )
 #NFA N1 from book page #48
 def NFA_N1():
     def transitionFunction(qi, c):
         if qi == "q0" and c == "0":
-            return tuple(["q0"])
+            return ["q0"]
         elif qi == "q0" and c == "1":
-            return tuple(["q0", "q1"])
-        elif qi == "q1" and (c == "0" or c == None):
-            return tuple(["q2"])
+            return ["q0", "q1"]
+        elif qi == "q1" and c == "0":
+            return ["q2"]
         elif qi == "q2" and c == "1":
-            return tuple(["q3"])
+            return ["q3"]
         elif qi == "q3" and (c == "0" or c == "1"):
-            return tuple(["q3"])
+            return ["q3"]
+    def epsilonFunction(qi):
+        if qi == "q1":
+            return ["q2"]
 
     return NFA(
         lambda qi: qi == "q0" or qi == "q1" or qi == "q2" or qi == "q3",
         ["0", "1"],
-        tuple(["q0"]),
+        "q0",
         transitionFunction,
-        lambda qi: qi == "q3"
+        lambda qi: qi == "q3",
+        epsilonFunction
     )
 
 #NFA N2 frome book page #51
 def NFA_N2():
     def transitionFunction(qi, c):
         if qi == "q0" and c == "0":
-            return tuple(["q0"])
+            return ["q0"]
         elif qi == "q0" and c == "1":
-            return tuple(["q0", "q1"])
+            return ["q0", "q1"]
         elif qi == "q1" and (c == "0" or c == "1"):
-            return tuple(["q2"])
+            return ["q2"]
         elif qi == "q2" and (c == "0" or c == "1"):
-            return tuple(["q3"])
-
+            return ["q3"]
+    def epsilonFunction(qi):
+        return []
     return NFA(
         lambda qi: qi == "q0" or qi == "q1" or qi == "q2" or qi == "q3",
         ["0", "1"],
-        tuple(["q0"]),
+        "q0",
         transitionFunction,
-        lambda qi: qi == "q3"
+        lambda qi: qi == "q3",
+        epsilonFunction
     )
 
 #NFA N4 frome book page #51
 def NFA_N4():
     def transitionFunction(qi, c):
+        if c == None:
+            return self.currentState
         if qi == "q0" and c == "b":
-            return tuple(["q1"])
-        elif qi == "q0" and c == None:
-            return tuple(["q2"])
+            return ["q1"]
+        elif qi == "q0":
+            return ["q2"]
         elif qi == "q1" and c == "a":
-            return tuple(["q1", "q2"])
+            return ["q1", "q2"]
         elif qi == "q1" and c == "b":
-            return tuple(["q2"])
+            return ["q2"]
         elif qi == "q2" and c == "a":
-            return tuple(["q0"])
-
+            return ["q0"]
+    def epsilonFunction(qi):
+        if qi == "q0":
+            return ["q2"]
     return NFA(
         lambda qi: qi == "q0" or qi == "q1" or qi == "q2",
         ["a", "b"],
-        tuple(["q0"]),
+        "q0",
         transitionFunction,
-        lambda qi: qi == "q0"
+        lambda qi: qi == "q0",
+        epsilonFunction
     )
 
 
 #NFA Three from end is one
 def NFA_ThreeFromEndIsOne():
     def transitionFunction(qi, c):
-        # If epsilon return currentState
-        if c == None:
-            return tuple([self.startState])
+        if qi == "0" and c == "0":
+            return ["0"]
+        elif qi == "0" and c == "1":
+            return ["0", "1"]
+        elif qi == "1":
+            return ["2"]
+        elif qi == "2":
+            return ["3"]
+        #* qi == 3 *
         else:
-            if qi == "0" and c == "0":
-                return tuple(["0"])
-            elif qi == "0" and c == "1":
-                return tuple(["0", "1"])
-            elif qi == "1":
-                return tuple(["2"])
-            elif qi == "2":
-                return tuple(["3"])
-            # return currentState
-            elif qi == "3":
-                return tuple([self.startState])
+            return []
+    def epsilonFunction(qi):
+        return []
     return NFA(
         lambda qi: qi == "0" or qi == "1" or qi == "2" or qi == "3",
         ["0", "1", "2", "3"],
         "0",
         transitionFunction,
-        lambda qi: qi == "3"
+        lambda qi: qi == "3",
+        epsilonFunction
+    )
+
+#NFA has epsilon
+def NFA_HasEps():
+    def transitionFunction(qi, c):
+        if qi == "0" and c == "0":
+            return ["0"]
+        elif qi == "1" and c == "0":
+            return ["0"]
+        elif qi == "1" and c == "1":
+            return ["2"]
+        elif qi == "2" and c == "0":
+            return ["1"]
+        elif qi == "2" and c == "1":
+            return ["2"]
+        else:
+            return []
+    def epsilonFunction(qi):
+        if qi == "0":
+            return ["1", "2"]
+        else:
+            return []
+
+    return NFA(
+        lambda qi: qi == "0" or qi == "1" or qi == "2",
+        ["0, 1"],
+        "0",
+        transitionFunction,
+        lambda qi: qi == "1",
+        epsilonFunction
     )
--- nfaTest.py
+++ nfaTest.py
@@ -10,72 +10,75 @@ NFA_N1 = nfa.NFA_N1()
 NFA_N2 = nfa.NFA_N2()
 NFA_N4 = nfa.NFA_N4()
 NFA_ThreeFromEndIsOne = nfa.NFA_ThreeFromEndIsOne()
+NFA_HasEps = nfa.NFA_HasEps()
 
 #*******TASK #26 ************
 #nfaTrace(NFA_Even, [], True, True)
 #nfaTrace(NFA_Odd, [], False, True)
-
+"""
 #NFA no strings
 #Always false
-oracle(NFA_NoStrings, [], False)
-oracle(NFA_NoStrings, ["0", "0"], False)
+print(nfa.oracle(NFA_NoStrings, [], False))
+print(nfa.oracle(NFA_NoStrings, ["0", "0"], False))
 
 #NFA Even length
-Print("NFA_Even_Length")
+print("NFA_Even_Length")
 #True
-oracle(NFA_Even_Length, ["q0", None], True)
-oracle(NFA_Even_Length, [], True)
-oracle(NFA_Even_Length, [["q1", "0"], ["q0", "1"]], True)
+print(nfa.oracle(NFA_Even_Length, [["q0"]], True))
+print(nfa.oracle(NFA_Even_Length, [], True))
+print(nfa.oracle(NFA_Even_Length, [["q1", "0"], ["q0", "1"]], True))
 #False
-oracle(NFA_Even_Length, [["q1", "0"]], False)
-oracle(NFA_Even_Length, [["q1", "0"],["q0", "1"],["q1", "0"]], False)
+print(nfa.oracle(NFA_Even_Length, [["q1", "0"]], False))
+print(nfa.oracle(NFA_Even_Length, [["q1", "0"],["q0", "1"],["q1", "0"]], False))
 
 #NFA Odd length
-Print("NFA_Odd_Length")
+print("NFA_Odd_Length")
 #True
-oracle(NFA_Odd_Length, [["q1", "0"]], True)
-oracle(NFA_Odd_Length, [["q1", "0"],["q0", "1"],["q1", "0"]], True)
+print(nfa.oracle(NFA_Odd_Length, [["q1", "0"]], True))
+print(nfa.oracle(NFA_Odd_Length, [["q1", "0"],["q0", "1"],["q1", "0"]], True))
 #False
-oracle(NFA_Odd_Length, [["q0", None]], False)
-oracle(NFA_Odd_Length, [], False)
-oracle(NFA_Odd_Length, [["q1", "0"], ["q0", "1"]], False)
+print(nfa.oracle(NFA_Odd_Length, [["q0"]], False))
+print(nfa.oracle(NFA_Odd_Length, [], False))
+print(nfa.oracle(NFA_Odd_Length, [["q1", "0"], ["q0", "1"]], False))
 
 #NFA N1
-Print("NFA_N1")
+print("NFA_N1")
 #True
-oracle(NFA_N1, [["q1", "1"], ["q2", "0"], ["q3", "1"]], True)
-oracle(NFA_N1, [["q1", "1"], ["q2", None], ["q3", "1"]], True)
-oracle(NFA_N1, [["q1", "1"], ["q2", "0"], ["q3", "1"], ["q3", "1"]], True)
+print(nfa.oracle(NFA_N1, [["q1" , "1"], ["q2", "0"], ["q3", "1"]], True))
+print(nfa.oracle(NFA_N1, [["q1" , "1"], ["q2"], ["q3", "1"]], True))
+print(nfa.oracle(NFA_N1, [["q1" , "1"], ["q2", "0"], ["q3", "1"], ["q3", "1"]], True))
 #False
-oracle(NFA_N1, [], False)
-oracle(NFA_N1, [["q0", "1"], ["q0", "0"], ["q0", "1"]], False)
-oracle(NFA_N1, [["q1", "1"],["q2", "0"]], False)
+print(nfa.oracle(NFA_N1, [], False))
+print(nfa.oracle(NFA_N1, [["q0" , "1"], ["q0", "0"], ["q0", "1"]], False))
+print(nfa.oracle(NFA_N1, [["q1" , "1"],["q2", "0"]], False))
+
 
 #NFA N2
-Print("NFA_N2")
+print("NFA_N2")
 #True
-oracle(NFA_N2, [["q1", "1"], ["q2", "0"], ["q3", "1"]], True)
-oracle(NFA_N2, [["q1", "1"], ["q2", "1"], ["q3", "0"]], True)
-oracle(NFA_N2, [["q0", "1"], ["q1", "1"], ["q2", "0"], ["q3", "1"]], True)
+print(nfa.oracle(NFA_N2, [["q1", "1"], ["q2", "0"], ["q3", "1"]], True))
+print(nfa.oracle(NFA_N2, [["q1", "1"], ["q2", "1"], ["q3", "0"]], True))
+print(nfa.oracle(NFA_N2, [["q0", "1"], ["q1", "1"], ["q2", "0"], ["q3", "1"]], True))
 #False
-oracle(NFA_N2, [], False)
-oracle(NFA_N2, [["q0", "1"], ["q0", "0"], ["q0", "1"]], False)
-oracle(NFA_N2, [["q0", "1"],["q1", "1"]], False)
+print(nfa.oracle(NFA_N2, [], False))
+print(nfa.oracle(NFA_N2, [["q0", "1"], ["q0", "0"], ["q0", "1"]], False))
+print(nfa.oracle(NFA_N2, [["q0", "1"],["q1", "1"]], False))
 
 #NFA N4
-Print("NFA_N4")
+print("NFA_N4")
 #True
-oracle(NFA_N4, [], True)
-oracle(NFA_N4, [["q2", None], ["q0", "a"]], True)
-oracle(NFA_N4, [["q1", "b"], ["q2", "a"], ["q0", "a"]], True)
+print(nfa.oracle(NFA_N4, [], True))
+print(nfa.oracle(NFA_N4, [["q2"], ["q0", "a"]], True))
+print(nfa.oracle(NFA_N4, [["q1", "b"], ["q2", "a"], ["q0", "a"]], True))
 #False
-oracle(NFA_N4, [["q1", "b"]], False)
-oracle(NFA_N4, [["q1", "b"], ["q2", "a"]], False)
-oracle(NFA_N4, [["q1", "b"], ["q1", "a"], ["q1", "a"]], False)
+print(nfa.oracle(NFA_N4, [["q1", "b"]], False))
+print(nfa.oracle(NFA_N4, [["q1", "b"], ["q2", "a"]], False))
+print(nfa.oracle(NFA_N4, [["q1", "b"], ["q1", "a"], ["q1", "a"]], False))
 
-#NFA ThreeFromEndIsOne
-Print("NFA_ThreeFromEndIsOne")
-oracle(NFA_ThreeFromEndIsOne, [["1", "0"], ["0", "1"], ["0", "1"], ["0", "0"], ["0", "1"]], False)
-oracle(NFA_ThreeFromEndIsOne, [["0", "0"], ["0", "1"], ["0", "1"], ["0", "0"], ["0", "1"]], True)
-oracle(NFA_ThreeFromEndIsOne, [["0", "0"], ["0", "1"], ["1", "1"], ["2", "0"], ["2", "1"]], True)
-oracle(NFA_ThreeFromEndIsOne, [["0", "0"], ["0", "1"], ["1", "1"], ["2", "0"], ["2", "1"]], False)
+#NFA HasEpsilon
+print(nfa.oracle(NFA_HasEps, [] , False))
+print(nfa.oracle(NFA_HasEps, [["1"]] , True))
+print(nfa.oracle(NFA_HasEps, [["2"]] , False))
+print(nfa.oracle(NFA_HasEps, [["0", "0"]] , False))
+print(nfa.oracle(NFA_HasEps, [["0", "0"], ["1"]] , True))
+"""
