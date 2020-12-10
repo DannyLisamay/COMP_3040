7e2632e425362a3bee5acca46931a9b7447c5185
Danny Lisamay <dannylisamay@gmail.com>
2020-12-10 01:35:11 -0500

    Task #42-44 Completed.

--- Checkpoint4_Test.py
+++ Checkpoint4_Test.py
@@ -2,7 +2,8 @@ import nfa
 import testDFA_2
 import nfaTest
 import dfa_ver2
-
+import regex
+"""
 NFA_NoStrings = nfa.NFA_NoStrings()
 NFA_Even_Length = nfa.NFA_Even_Length()
 NFA_Odd_Length = nfa.NFA_Odd_Length()
@@ -22,3 +23,71 @@ DFA_N4 = nfa.nfa2dfa(NFA_N4)
 DFA_ThreeFromEndIsOne = nfa.nfa2dfa(NFA_ThreeFromEndIsOne)
 DFA_HasEps = nfa.nfa2dfa(NFA_HasEps)
 DFA_OneBB = nfa.nfa2dfa(NFA_OneBB)
+"""
+
+#*******TASK #43 ************
+#Write a example regular expressions.
+#Basic regex
+emptyR = regex.Regex_Empty()
+print(emptyR)
+epsilonR = regex.Regex_Epsilon()
+print(epsilonR)
+
+#Example from inclass
+rbin = regex.Regex_Union(regex.Regex_Char(0), regex.Regex_Char(1))
+ronebb = regex.Regex_Circ(regex.Regex_Star(rbin), regex.Regex_Circ(rbin,rbin))
+print(rbin)
+print(ronebb)
+
+#Third from end is one
+# Left Hand
+#(0 U 1)
+zeroUone = regex.Regex_Union(regex.Regex_Char(0),regex.Regex_Char(1))
+print(zeroUone)
+
+#(0 U 1)*
+starZeroUOne = regex.Regex_Star(zeroUone)
+print(starZeroUOne)
+
+#Right Hand
+#1
+one = regex.Regex_Char(1)
+#1(((0 U 1) C (0 U 1)))
+rh = regex.Regex_Circ(one, regex.Regex_Union(zeroUone,zeroUone))
+print(rh)
+#RE...
+thirdFromEndIsOne = regex.Regex_Circ(starZeroUOne, rh)
+print(thirdFromEndIsOne)
+
+#Example from textbook 1.56
+a = regex.Regex_Char("a")
+b = regex.Regex_Char("b")
+ab = regex.Regex_Circ(a,b)
+abUa = regex.Regex_Union(ab, a)
+abUaStar = regex.Regex_Star(abUa)
+print(a)
+print(b)
+print(ab)
+print(abUa)
+print(abUaStar)
+
+#*******TASK #43 ************
+# Basic regex
+#a should accept a
+#b should accept b
+#ab should accept and b
+#abUa should accept a and b or a
+
+#thirdFromEndIsOne
+#pass
+#100
+#0100
+#111
+#101
+#fail
+#000
+#1
+#0
+#011
+
+#ronebb
--- regex.py
+++ regex.py
@@ -1,29 +1,57 @@
 #*******TASK #41 ************
 #Define a data type to represent regular expressions.
+#Objects
+#*******TASK #42 ************
+#Write a printer for regular expressions.
+#toString format __str__
 #Empty
 class Regex_Empty:
     def __init__(self):
-		pass
+        pass
+    #toString format
+    def __str__(self):
+        return "Empty "
 #Epsilon
 class Regex_Epsilon:
     def __init__(self):
-		pass
+        pass
+    #toString format
+    def __str__(self):
+        return "Epsilon "
 #Char
 class Regex_Char:
     def __init__(self, c):
-		self.c = c
+        self.c = c
+    #toString format
+    def __str__(self):
+        return str(self.c)
 #Union
 class Regex_Union:
     def __init__(self, left, right):
-		self.l = left
-		self.r = right
+        self.l = left
+        self.r = right
+    #toString format
+    def __str__(self):
+        return f"({self.l} U {self.r})"
 #Star
-class Regex_Star
+class Regex_Star:
     def __init__(self, x):
-		self.x = x
+        self.x = x
+    #toString format
+    def __str__(self):
+        return f"({self.x} *"
 
 #Circ
 class Regex_Circ:
     def __init__(self, left, right):
-		self.l = left
-		self.r = right
+        self.l = left
+        self.r = right
+    #toString format
+    def __str__(self):
+        return f"({self.l} C {self.r})"
+
+#*******TASK #45 ************
+#Write a function that accepts a regular expressions and generates
+#a random string that would be accepted by it.
+def generator(r):
+    return []
